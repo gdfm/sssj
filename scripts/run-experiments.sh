e0833ca5b919ca3d554b54f81dc6cb6b03989c22
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-09-09 18:02:33 +0300

    Fix output redirection for experiments

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -8,5 +8,5 @@ DATA="data/RCV1_seq.bin"
 THETA="0.35 0.5 0.7 0.8 0.9 0.99"
 LAMBDA="1 0.1 0.01 0.001 0.0001"
 INDEX="INVERTED ALL_PAIRS L2AP"
-parallel --max-procs ${max_procs} "scripts/minibatch -t {1} -l {2} -f ${FORMAT} ${DATA} ${cliopts}; echo" ::: $THETA ::: $LAMBDA > "results/t${t}_l${l}_mb-inv"
-parallel --max-procs ${max_procs} "scripts/streaming -t {1} -l {2} -f ${FORMAT} ${DATA} ${cliopts}; echo" ::: $THETA ::: $LAMBDA > "results/t${t}_l${l}_str"
+parallel --max-procs ${max_procs} "scripts/minibatch -t {1} -l {2} -f ${FORMAT} ${DATA} ${cliopts} > results/t{1}_l{2}_mb-inv" ::: $THETA ::: $LAMBDA
+parallel --max-procs ${max_procs} "scripts/streaming -t {1} -l {2} -f ${FORMAT} ${DATA} ${cliopts} > results/t{1}_l{2}_str   " ::: $THETA ::: $LAMBDA
