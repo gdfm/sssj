524ca74e97c6f93d053331c2a7721b501f004336
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-09-14 11:06:35 +0300

    Ungroup parallel output

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -8,5 +8,5 @@ THETA="0.35 0.5 0.7 0.8 0.9 0.99"
 LAMBDA="1 0.1 0.01 0.001 0.0001"
 INDEX="INVERTED ALL_PAIRS L2AP"
 
-parallel --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_mb-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
-parallel --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} ${cliopts} > results/{1/.}_t{2}_l{3}_STREAMING" ::: $DATA ::: $THETA ::: $LAMBDA
+parallel --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_mb-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
+parallel --ungroup --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} ${cliopts} > results/{1/.}_t{2}_l{3}_STREAMING" ::: $DATA ::: $THETA ::: $LAMBDA
