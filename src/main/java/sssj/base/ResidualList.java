d1863320b63a057188fcd9bb7813f6a74f170b71
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-09-22 11:29:35 +0300

    Add missing ResidualList class

---
+++ src/main/java/sssj/base/ResidualList.java
@@ -0,0 +1,49 @@
+package sssj.base;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ResidualList implements Iterable<Vector> {
+  private Queue<Vector> queue = new LinkedList<>();
+
+  public void add(Vector residual) {
+    queue.add(residual);
+  }
+
+  @Override
+  public Iterator<Vector> iterator() {
+    return queue.iterator();
+  }
+
+  @Override
+  public String toString() {
+    return "ResidualList = [" + queue + "]";
+  }
+
+  // TODO should use a map for MiniBatch?
+  public Vector get(long candidateID) {
+    for (Vector v : queue)
+      if (candidateID == v.timestamp())
+        return v;
+    return null;
+  }
+
+  /**
+   * Get the residual of the vector with id {@code candidateID}, while at the same time pruning all the residuals with timestamp less than {@code lowWatermark}.
+   *
+   * @param candidateID the id of the candidate vector
+   * @param lowWatermark the minimum id of the vector to be retained
+   * @return the residual of the candidate
+   */
+  public Vector getAndPrune(long candidateID, long lowWatermark) {
+    for (Iterator<Vector> it = queue.iterator(); it.hasNext();) {
+      final Vector v = it.next();
+      if (lowWatermark > v.timestamp())
+        it.remove();
+      if (candidateID == v.timestamp())
+        return v;
+    }
+    return null;
+  }
+}
