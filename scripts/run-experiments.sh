19342a99a93f0ae92e69ff89f0470268a556fb3e
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-09-24 12:01:45 +0300

    Add index option to Streaming

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -3,10 +3,11 @@
 cliopts="$@"
 max_procs=1
 
-DATA="data/RCV1_seq.bin"
+DATA="data/RCV1-seq.bin"
 THETA="0.35 0.5 0.7 0.8 0.9 0.99"
 LAMBDA="1 0.1 0.01 0.001 0.0001"
-INDEX="INVERTED ALLPAIRS L2AP"
+#INDEX="INVERTED ALLPAIRS L2AP"
+INDEX="INVERTED L2AP"
 
-parallel --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_mb-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
-parallel --ungroup --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} ${cliopts} > results/{1/.}_t{2}_l{3}_STREAMING" ::: $DATA ::: $THETA ::: $LAMBDA
+parallel --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_MB-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
+parallel --ungroup --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_STR-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
