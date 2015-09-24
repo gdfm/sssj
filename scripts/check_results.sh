7039a1ba05c238be235408ce147a7ba944eec224
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-09-24 11:59:48 +0300

    Avoid checking results against themselves

---
+++ scripts/check_results.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+RES_DIR="results"
+# results file name format: DATASET_THETA_LAMBDA_ALGO-INDEX
+confs=$(ls $RES_DIR | cut -d '_' -f 1-3 | sort | uniq)
+algos=$(ls $RES_DIR | cut -d '_' -f 4 | sort | uniq)
+
+parallel -u -j0 "[[ \"{2}\" -eq \"{3}\" ]] || scripts/diff.py $RES_DIR/{1}_{2} $RES_DIR/{1}_{3}" ::: $confs ::: $algos ::: $algos
