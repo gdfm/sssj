2b6910f7862c6cc6873905d38128c4b04f45ca22
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-11-16 11:09:33 +0200

    Setup experiments

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -3,12 +3,13 @@
 cliopts="$@"
 max_procs=2
 
-DATA="data/RCV1-seq.bin"
+DATA="data/rcv1-seq.bin data/webspam-poi.bin data/blogs-06.bin data/tweets-06.bin"
 THETA="0.35 0.5 0.7 0.8 0.9 0.99"
-LAMBDA="1 0.1 0.01 0.001 0.0001"
+LAMBDA="0.1 0.01 0.001 0.0001"
 
 #INDEX="INVERTED ALLPAIRS L2AP"
-#parallel --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_MB-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
+INDEX="INVERTED L2AP PUREL2AP"
+parallel --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_MB-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
 
 INDEX="INVERTED PUREL2AP"
 parallel --ungroup --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_STR-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
