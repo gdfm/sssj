9fb067e735ded57fab4ef547095ea27e39ba61f6
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-11-19 16:14:20 +0200

    More robust script to check results (ignore temp files)

--- scripts/check_results.sh
+++ scripts/check_results.sh
@@ -2,7 +2,7 @@
 
 RES_DIR="results"
 # results file name format: DATASET_THETA_LAMBDA_ALGO-INDEX
-confs=$(ls $RES_DIR | cut -d '_' -f 1-3 | sort | uniq)
-algos=$(ls $RES_DIR | cut -d '_' -f 4 | sort | uniq)
+confs=$(ls $RES_DIR | grep '_' | cut -d '_' -f 1-3 | sort | uniq)
+algos=$(ls $RES_DIR | grep '_' | cut -d '_' -f 4   | sort | uniq)
 
 parallel -u -j0 "[[ \"{2}\" -eq \"{3}\" ]] || scripts/diff.py $RES_DIR/{1}_{2} $RES_DIR/{1}_{3}" ::: $confs ::: $algos ::: $algos
