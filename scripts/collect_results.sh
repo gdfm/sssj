3dff5a0d15123dc333113213b121c67cc1736e06
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-11-23 11:24:00 +0200

    Ignore partially collected results

--- scripts/collect_results.sh
+++ scripts/collect_results.sh
@@ -3,4 +3,4 @@
 HEADER="algorithm, dataset, theta, lambda, index, time, entries, candidates, similarities, matches"
 OUTFILE="results/results.csv"
 echo $HEADER > $OUTFILE
-tail -q -n 1 results/*_* | grep -v '~' >> $OUTFILE
+tail -q -n 1 results/*_* | grep -ve '~' -ve '=' >> $OUTFILE
