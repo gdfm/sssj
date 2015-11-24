bae08e84094fab17cd58664377e506811895f112
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-11-24 14:52:31 +0200

    Add timeout to experiments

--- scripts/run-experiments.sh
+++ scripts/run-experiments.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cliopts="$@"
-max_procs=1
+cliopts="$@"	# optional parameters
+max_procs=1	# parallelism
+timeout=7200	# timeout in seconds
 
 DATA="data/rcv1-seq.bin data/webspam-poi.bin data/blogs-06.bin data/tweets-06.bin"
 THETA="0.5 0.7 0.8 0.9 0.99"
@@ -9,8 +10,8 @@ LAMBDA="0.1 0.01 0.001 0.0001"
 
 #INDEX="INVERTED ALLPAIRS L2AP PUREL2AP"
 INDEX="INVERTED L2AP PUREL2AP"
-parallel --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_MB-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
+parallel --timeout ${timeout} --ungroup --max-procs ${max_procs} "scripts/minibatch {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_MB-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
 
 #INDEX="INVERTED L2AP PUREL2AP"
 INDEX="INVERTED PUREL2AP"
-parallel --ungroup --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_STR-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
+parallel --timeout ${timeout} --ungroup --max-procs ${max_procs} "scripts/streaming {1} -t {2} -l {3} -i {4} ${cliopts} > results/{1/.}_t{2}_l{3}_STR-{4}" ::: $DATA ::: $THETA ::: $LAMBDA ::: $INDEX
